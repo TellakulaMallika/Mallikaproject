2fefeb405bfcb60ca5d317db7d3df380b90ca6bc
T.Mallika <tellakulamallika@gmailc.om>
2024-02-24 18:37:24 -0700

    firstcommit

---
+++ PizzaManagement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class PizzaManagement
+    {
+        static List<Pizza> pizzas = new List<Pizza>()
+        {
+            new Pizza{Id=1,Name="Paneer Pizza",Price=350},
+            new Pizza{Id=2,Name="Mushroom Pizza",Price=450},
+            new Pizza{Id=3,Name="Corn Pizza",Price=300},
+        };
+        public List<Pizza> AllPizzas()
+        {
+            return pizzas;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp5;
+using System.Security;
+Console.WriteLine("Joes's Pizza details");
+PizzaManagement pm = new PizzaManagement();
+foreach(var p in pm.AllPizzas())
+{
+    Console.WriteLine(p.Id);
+    Console.WriteLine(p.Name);
+    Console.WriteLine(p.Price);
+}
